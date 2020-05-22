fafe9a82436a32d47a33f5e5f86c68b57a9a32f5
Thuy.Copeland <thuycopeland@gmail.com>
2020-05-22 15:50:25 -0500

    Create the AuthBaseTableViewController

---
+++ WordPressAuthenticator/Unified Auth/View Related/AuthBaseTableViewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+
+class AuthBaseTableViewController: UITableViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+}
