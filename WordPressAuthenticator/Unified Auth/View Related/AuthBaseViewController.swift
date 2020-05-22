5717b5941a272805d4745c33e4cb5a99b11b727f
Thuy.Copeland <thuycopeland@gmail.com>
2020-05-22 15:37:45 -0500

    Add AuthBaseViewController

---
+++ WordPressAuthenticator/Unified Auth/View Related/AuthBaseViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+
+/// The base view controller for the Unified Auth flows.
+///
+class AuthBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
