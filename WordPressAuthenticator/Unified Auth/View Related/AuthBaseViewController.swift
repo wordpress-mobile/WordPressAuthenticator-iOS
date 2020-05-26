aa9822ae61b34789b85b9a827f8500db1f3f3c28
Thuy.Copeland <thuycopeland@gmail.com>
2020-05-26 15:58:00 -0500

    Add a tableView to the AuthBase UIViewController

--- WordPressAuthenticator/Unified Auth/View Related/AuthBaseViewController.swift
+++ WordPressAuthenticator/Unified Auth/View Related/AuthBaseViewController.swift
@@ -1,14 +1,13 @@
 import UIKit
 
 
-/// The base view controller for the Unified Auth flows.
+/// AuthBaseViewController: the base view controller for the Unified Auth flows.
 ///
 class AuthBaseViewController: UIViewController {
+    @IBOutlet var formButton: UIButton!
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
 }
