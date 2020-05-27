f832b545d49b9ca6a469b7e14722765f412edfc1
Thuy.Copeland <thuycopeland@gmail.com>
2020-05-27 14:20:48 -0500

    Add some comments

--- WordPressAuthenticator/Unified Auth/View Related/AuthBaseViewController.swift
+++ WordPressAuthenticator/Unified Auth/View Related/AuthBaseViewController.swift
@@ -4,9 +4,15 @@ import UIKit
 /// AuthBaseViewController: the base view controller for the Unified Auth flows.
 ///
 class AuthBaseViewController: UIViewController {
+    /// The "call to action" button.
+    ///
     @IBOutlet var formButton: UIButton!
+
+    /// The main tableView.
+    ///
     @IBOutlet var tableView: UITableView!
 
+    // MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
