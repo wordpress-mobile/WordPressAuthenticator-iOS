749b229c45bbfb2ce409b2af9e19907f4b6ac593
Thuy.Copeland <thuycopeland@gmail.com>
2020-09-29 16:23:15 -0500

    Explain why an API failure is still a success

--- WordPressAuthenticator/Services/LoginFacade.swift
+++ WordPressAuthenticator/Services/LoginFacade.swift
@@ -20,8 +20,11 @@ extension LoginFacade {
 
                 completion(nil, true)
         }) { error in
-            DDLogError("Failed to request one time code")
-            completion(error, false)
+            /// So these new completion handlers allow API request failure messages to bubble up to the user layer.
+            /// However, we receive a false negative in this situation. The API reports a failure, but still successfully
+            /// sends an SMS OTP. So we'll log it, but not report the error to the user.
+            DDLogError("Possible failure when requesting a one time code: \(error?.localizedDescription)")
+            completion(nil, true)
         }
     }
 
