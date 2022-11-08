268e2628a77289f94600de9932751af1bf0f9db7
Sharma Elanthiraiyan <sharma.elanthiraiyan@automattic.com>
2022-11-08 16:57:05 +0530

    Move XMLRPC error message logic to extension reuse code.

--- WordPressAuthenticator/Extensions/NSError+XMLRPC.swift
+++ WordPressAuthenticator/Extensions/NSError+XMLRPC.swift
@@ -1,5 +1,8 @@
 import Foundation
+import WordPressKit
 
+/// Helper methods for XMLRPC validation related errors
+///
 extension NSError {
     func originalErrorOrError() -> NSError {
         guard let err = userInfo[XMLRPCOriginalErrorKey] as? NSError else {
@@ -7,5 +10,19 @@ extension NSError {
         }
 
         return err
+    }
+
+    func errorMessage() -> String? {
+        if let xmlrpcValidatorError = self as? WordPressOrgXMLRPCValidatorError {
+            return xmlrpcValidatorError.localizedDescription
+        } else if (domain == NSURLErrorDomain && code == NSURLErrorCannotFindHost) ||
+                    (domain == NSURLErrorDomain && code == NSURLErrorNetworkConnectionLost) {
+            // NSURLErrorNetworkConnectionLost can be returned when an invalid URL is entered.
+           return NSLocalizedString(
+                "The site at this address is not a WordPress site. For us to connect to it, the site must use WordPress.",
+                comment: "Error message shown a URL does not point to an existing site.")
+        }
+
+        return nil
     }
 }
