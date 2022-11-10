c3b3bccfc24b8343f285d8b8a1fe234a1814700a
Gio Lodi <gio@mokacoding.com>
2022-11-10 09:51:00 +0100

    Set `window` in demo app `AppDelegate` to avoid SVProgressHUD crash

--- Demo/AuthenticatorDemo/AppDelegate.swift
+++ Demo/AuthenticatorDemo/AppDelegate.swift
@@ -3,6 +3,13 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    // This is a property defined in `UIApplicationDelegate`. In modern UIKit apps,
+    // `UISceneConfiguration` is responsible for creating and holding the window.
+    //
+    // However, we need to set this anyway because SVProgressHUD accesses it internally and will
+    // crash the app if the value it finds is nil.
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
