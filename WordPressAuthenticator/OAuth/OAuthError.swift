d8b1169706171632a9d4fd04275c775a1828d39d
Gio Lodi <gio@mokacoding.com>
2022-12-02 15:48:17 +0100

    Throw errors from `asURLEncodedData()`

--- WordPressAuthenticator/OAuth/OAuthError.swift
+++ WordPressAuthenticator/OAuth/OAuthError.swift
@@ -3,4 +3,16 @@ enum OAuthError {
     struct InconsistentASWebAuthenticationSessionCompletion: LocalizedError {
         let errorDescription = "ASWebAuthenticationSession authentication finished with neither a callback URL nor error"
     }
+
+    struct FailedToBuildURLQuery: LocalizedError {
+        let requestBody: OAuthTokenRequestBody
+
+        lazy var errorDescription = "Failed to build URL query string from \(requestBody)"
+    }
+
+    struct FailedToEncodeURLQuery: LocalizedError {
+        let query: String
+
+        lazy var errorDescription = "Failed to encode URL query string '\(query)'"
+    }
 }
