9cffc11e3b98c263d351dc48e8b186942a851c89
Gio Lodi <gio@mokacoding.com>
2022-12-05 15:23:06 +0100

    Tentatively add "New" `NewGoogleAuthenticator`

--- WordPressAuthenticator/OAuth/OAuthError.swift
+++ WordPressAuthenticator/OAuth/OAuthError.swift
@@ -3,10 +3,15 @@ enum OAuthError: LocalizedError {
     // ASWebAuthenticationSession
     case inconsistentWebAuthenticationSessionCompletion
 
+    // OAuth token response
+    case urlDidNotContainCodeParameter(url: URL)
+
     var errorDescription: String {
         switch self {
         case .inconsistentWebAuthenticationSessionCompletion:
             return "ASWebAuthenticationSession authentication finished with neither a callback URL nor error"
+        case .urlDidNotContainCodeParameter(let url):
+            return "Could not find 'code' parameter in URL '\(url)'"
         }
     }
 }
