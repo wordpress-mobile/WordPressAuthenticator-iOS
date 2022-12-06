7c8a21395ffde3b2cbe0186abaaeae373162f227
Gio Lodi <gio@mokacoding.com>
2022-12-06 12:10:22 +0100

    Pull new `Secrets.swift` version with credentials for UI testing

--- Demo/AuthenticatorDemo/Secrets.example.swift
+++ Demo/AuthenticatorDemo/Secrets.example.swift
@@ -9,3 +9,20 @@ enum APICredentials {
     static let googleLoginSchemeId = ""
     static let googleLoginServerClientId = ""
 }
+
+// In a proper app, we'd split production secrets from test secrets in two
+// files and include each only in the appropriate target(s).
+//
+// Given this is a demo app, we can be a bit less clean in the interest of
+// simplicity and use a single file to include in both application and test
+// targets.
+extension APICredentials {
+
+    enum Tests {
+
+        enum GoogleAccount {
+            static let email = ""
+            static let password = ""
+        }
+    }
+}
