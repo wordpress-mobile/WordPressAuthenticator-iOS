4146597db523bf0cb9bea370992b78f128fb9061
Gio Lodi <gio@mokacoding.com>
2022-12-07 11:00:45 +0100

    Run UI tests in CI

--- .buildkite/ui-test-demo-app.sh
+++ .buildkite/ui-test-demo-app.sh
@@ -6,5 +6,5 @@ install_gems
 echo "--- :cocoapods: Install Pods"
 install_cocoapods
 
-echo "--- :calling: Build Demo App"
-bundle exec fastlane build_demo_app
+echo "--- :calling: UI Test Demo App"
+bundle exec fastlane ui_test_demo_app
