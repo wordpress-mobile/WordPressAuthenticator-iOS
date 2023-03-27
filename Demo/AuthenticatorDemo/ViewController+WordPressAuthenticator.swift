e8f7d7b9c79d182ed1f19c8c7ed8a7b1b1965388
Gio Lodi <gio@mokacoding.com>
2023-03-27 23:01:47 +1100

    Handle user canceled "error" in demo app

--- Demo/AuthenticatorDemo/ViewController+WordPressAuthenticator.swift
+++ Demo/AuthenticatorDemo/ViewController+WordPressAuthenticator.swift
@@ -1,3 +1,4 @@
+import AuthenticationServices
 import WebKit
 import WordPressAuthenticator
 import WordPressKit
@@ -96,6 +97,12 @@ extension ViewController {
                 )
             } catch let error as OAuthError {
                 presentAlert(title: "❌", message: error.errorDescription, onDismiss: {})
+            } catch let error as NSError where
+                error.domain == ASWebAuthenticationSessionError.errorDomain
+                && error.code == ASWebAuthenticationSessionError.Code.canceledLogin.rawValue {
+                // In a production app, the UX would be better if we didn't present an alert.
+                // But here, it's useful to show it to make the error handling visible for reference.
+                presentAlert(title: "", message: "User cancelled", onDismiss: {})
             } catch {
                 fatalError("Caught an error that was not of the expected `OAuthError` type: \(error)")
             }
